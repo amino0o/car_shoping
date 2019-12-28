3c2d199add8ecdc689fc01453dc8d947de0324fb
Amin Golbaz <golbaz.amin@gmail.com>
2019-12-28 15:44:17 +0330

    updating our app with a shop page, collection preview component add collection item component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component'
 import { Switch ,Route , Link} from 'react-router-dom'
+import ShopPage from './pages/shoppage/shoppage.component'
 
-const Hats = (props) => {
-    console.log(props)
-    return(
-        <div>
-
-            <button onClick={() =>props.history.push('/')}>Home</button>
-            Hats
-        </div>
-    )
-}
+// const Hats = (props) => {
+//     console.log(props);
+//     return(
+//         <div>
+//
+//             <button onClick={() =>props.history.push('/')}>Home</button>
+//             Hats
+//         </div>
+//     )
+// }
 
 function App() {
   return (
     <div>
         <Switch>
             <Route exact path='/' component={Homepage} />
-            <Route path='/shop/hats' component={Hats} />
+            <Route path='/shop' component={ShopPage} />
         </Switch>
     </div>
   );
